fa3f24b9cda138e2a454d1d11b412478771a1c78
abhishekY2401 <abhiyd2401@gmail.com>
2024-10-04 01:13:44 +0530

    graphql setup using ariadne

---
+++ app/__init__.py
@@ -0,0 +1,28 @@
+# app/__init__.py
+from flask import Flask  # type: ignore
+from config import Config
+from app.extensions import db, jwt, bcrypt, migrate
+from app.graphql import graphql_server, graphql_playground
+import logging
+
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    # Initialize extensions with app
+    db.init_app(app)
+    jwt.init_app(app)
+    bcrypt.init_app(app)
+    migrate.init_app(app, db)
+
+    # Register GraphQL routes
+    app.add_url_rule('/graphql_playground',
+                     view_func=graphql_playground, methods=['GET'])
+    app.add_url_rule('/graphql', view_func=graphql_server, methods=['POST'])
+
+    # Set up logging
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s - %(levelname)s - %(message)s')
+
+    return app
---
+++ app/graphql.py
@@ -0,0 +1,31 @@
+from flask import request, jsonify
+from ariadne import load_schema_from_path, make_executable_schema, graphql_sync
+from ariadne.explorer.apollo import APOLLO_HTML
+from app.resolvers import query, mutation
+from flask_jwt_extended import jwt_required
+from app.middleware import jwt_auth_middleware
+import logging
+
+# Load the schema and create the executable schema
+type_defs = load_schema_from_path("app/schema.graphql")
+schema = make_executable_schema(type_defs, query, mutation)
+
+
+def graphql_server():
+    data = request.get_json()
+
+    success, result = graphql_sync(
+        schema,
+        data,
+        context_value=request,
+        # middleware=[jwt_auth_middleware],
+        debug=True
+    )
+
+    status_code = 200 if success else 400
+    return jsonify(result), status_code
+
+
+def graphql_playground():
+    logging.info("Accessing GraphQL Playground")
+    return APOLLO_HTML, 200
