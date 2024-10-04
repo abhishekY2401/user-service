9cfd3d812b7afff20b01fe72f785b41c6276d3b3
abhishekY2401 <abhiyd2401@gmail.com>
2024-10-05 01:21:43 +0530

    implement functions to publish and consume events from queue

--- app/rabbitmq.py
+++ app/rabbitmq.py
@@ -15,7 +15,7 @@ def setup_rabbitmq_connection():
 # publish an event to queue
 
 
-def publish_event(queue_name, message):
+def publish_event(exchange_name, routing_key, message):
     connection = setup_rabbitmq_connection()
 
     if not connection:
@@ -23,36 +23,49 @@ def publish_event(queue_name, message):
 
     try:
         channel = connection.channel()
-        channel.queue_declare(queue=queue_name, durable=True)
+        channel.exchange_declare(
+            exchange=exchange_name, exchange_type='direct')
         channel.basic_publish(
-            exchange='',
-            routing_key=queue_name,
+            exchange=exchange_name,
+            routing_key=routing_key,
             body=message,
             properties=pika.BasicProperties(
                 delivery_mode=2
             )
         )
-        logging.info(f"Published message to {queue_name}: {message}")
-
+        logging.info(
+            f"Published message to exchange {exchange_name} with routing key {routing_key}: {message}")
     finally:
         connection.close()
 
 
 # listen to events sent from different microservice
-def listen_events(queue_name, callback):
+def consume_events(on_message_received):
     connection = setup_rabbitmq_connection()
 
     if not connection:
         return None
 
-    channel = connection.channel()
-    channel.queue_declare(queue=queue_name, durable=True)
+    try:
+        channel = connection.channel()
+        channel.exchange_declare(
+            exchange='event_exchange', exchange_type='direct')
+        channel.queue_declare(queue='order_service_queue')
 
-    def on_message(channel, method, properties, body):
-        logging.info(f"Received message from {queue_name}: {body}")
-        callback(body)
-        channel.basic_ack(delivery_tag=method.delivery_tag)
+        # bind the queues to routing keys for listening to user and product related events
+        channel.queue_bind(exchange='event_exchange',
+                           queue='order_service_queue', routing_key='user.registered')
+        channel.queue_bind(exchange='event_exchange',
+                           queue='order_service_queue', routing_key='user.profile.updated')
+        channel.queue_bind(exchange='event_exchange',
+                           queue='order_service_queue', routing_key='product.created')
 
-    channel.basic_consume(queue=queue_name, on_message_callback=on_message)
-    logging.info(f"Listening for messages on {queue_name}...")
-    channel.start_consuming()
+        # Set up the consumer to listen for messages
+        channel.basic_consume(queue='order_service_queue',
+                              on_message_callback=on_message_received, auto_ack=True)
+        logging.info(
+            "Waiting for `user.updated` and `product.created` events. To exit press CTRL+C")
+        channel.start_consuming()
+
+    finally:
+        connection.close()
