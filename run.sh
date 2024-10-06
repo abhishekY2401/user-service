d75bf2f62c7be2dbefbb2da9b2691fda17ee9e38
abhishekY2401 <abhiyd2401@gmail.com>
2024-10-06 15:56:10 +0530

    update: running consumer file

--- run.sh
+++ run.sh
@@ -1,5 +1,12 @@
-# run.sh
 #!/bin/sh
 
+echo "Applying database migrations..."
 python3 -m flask db upgrade
-exec python3 main.py
+
+echo "Starting the Flask application..."
+python3 main.py &  # Start Flask app in background
+
+echo "Starting the consumer..."
+python3 consumer.py &  # Start consumer in background
+
+wait  # Wait for all background processes to finish
